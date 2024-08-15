618ccd3ac2b93b12972b40207a9a9373b24a6c92
Jake Pelrah <pelrahjsp@gmail.com>
2024-08-15 15:55:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function genRandomNum(n = 100) {
+    return Math.trunc(Math.random() * n + 1)
+}
+
+
+function guessNumber(randomNum, attempts = 10) {
+
+    let msg = ''
+    let guess = parseInt(prompt('Please guess a number from 1 to 100'))
+
+    while (guess !== randomNum && attempts > 1) {
+
+        if (isNaN(guess)) {
+            msg = 'Value is not a number try again'
+        }
+        else {
+            attempts--
+        }
+
+        if (guess < randomNum) {
+            msg = `Guess is less than the random number, you have ${attempts} attempts remaining`
+        }
+        else if (guess > randomNum) {
+            msg = `Guess is greater than the random number you have ${attempts} attempts remaining`
+        }
+
+        window.alert(msg)
+        guess = parseInt(prompt('Please guess a number from 1 to 100'))
+    }
+
+    return attempts > 1 ? `You guessed ${randomNum} correctly in ${attempts} attempts.` : 'You used all of your attempts, try again!'
+
+}23
+
+window.alert(guessNumber(genRandomNum()))
